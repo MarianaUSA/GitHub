6802671925625a184abec0ed2c973aaadd621ebb
MarianaUSA <mariana.petkova@hotmail.com>
2014-07-23 22:38:35 +0300

    added new products

--- papki/products_add.php
+++ papki/products_add.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	redirect('products.php');
 }
-$data=products_get($_GET['id']);
+//$data=products_get($_GET['id']);
 require_once('include/header.php');
 
 ?>
